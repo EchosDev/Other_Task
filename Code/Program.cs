0d1cd39e379bac9daa575b524fa8f5189eefbdfa
EchosDev <elmirfsh-apa202@code.edu.az>
2026-03-16 17:33:57 +0400

    Project Created

---
+++ Code/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Elmir");
+            person1.ShowInfo();
+        }
+    }
+
+    class Person
+    {
+        public string Name;
+        public string Surname;
+        public byte Age;
+        public bool IsMarried;
+
+        public Person(string name = "John", string surname = "Doe", byte age = 0, bool isMarried = false)
+        {
+            Name = name;
+            Surname = surname;  
+            Age = age;
+            IsMarried = isMarried;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Surname: {Surname}, Age: {Age}, IsMarried: {IsMarried}");
+        }
+    }
+}
